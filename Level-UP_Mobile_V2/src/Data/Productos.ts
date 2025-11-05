ad15a053be3da293d8a40f9772d20a1773dfd5ea
Sicr0911 <seb.catalanr@duocuc.cl>
2025-11-05 12:17:26 -0300

    arreglo

--- Level-UP_Mobile_V2/src/Data/Productos.ts
+++ Level-UP_Mobile_V2/src/Data/Productos.ts
@@ -1,4 +1,14 @@
 import type { Producto } from '../Interfaces/Producto';
+import audifonos from '../IMG/Audifonos.png';
+import catan from '../IMG/juegoCatan.png';
+import carcassonne from '../IMG/juegoCarcassonne.png';
+import mandoXbox from '../IMG/mandoXbox.png';
+import mouse from '../IMG/mouse.png';
+import mousepad from '../IMG/mousepad.png';
+import pc from '../IMG/pc.png';
+import ps5 from '../IMG/ps5.png';
+import silla from '../IMG/silla_gamer.png';
+import polera from '../IMG/polera.png';
 
 export const PRODUCTOS: Producto[] = [
   {
@@ -7,7 +17,16 @@ export const PRODUCTOS: Producto[] = [
     nombre: 'Catan',
     precio: 29990,
     descripcion: 'Un clásico juego de estrategia donde los jugadores compiten por colonizar y expandirse en la isla de Catan. Ideal para 3-4 jugadores y perfecto para noches de juego en familia o con amigos.',
-    imagen: 'Level-UP_Mobile_V2\src\IMG\juegoCatan.png'
+    imagen: catan
+  },
+
+  {
+    codigo: 'AU001',
+    categoria: 'Audifonos',
+    nombre: 'Auriculares Gamer HyperX Cloud II',
+    precio: 79990,
+    descripcion : 'Proporcionan un sonido envolvente de calidad con un micrófono desmontable y almohadillas de espuma viscoelástica para mayor comodidad durante largas sesiones de juego.',
+    imagen: audifonos
   },
 
   {
@@ -15,7 +34,8 @@ export const PRODUCTOS: Producto[] = [
     categoria: 'Juegos de Mesa',
     nombre: 'Carcassonne',
     precio: 24990,
-    descripcion : 'Un juego de colocación de fichas donde los jugadores construyen el paisaje alrededor de la fortaleza medieval de Carcassonne. Ideal para 2-5 jugadores y fácil de aprender.'
+    descripcion : 'Un juego de colocación de fichas donde los jugadores construyen el paisaje alrededor de la fortaleza medieval de Carcassonne. Ideal para 2-5 jugadores y fácil de aprender.',
+    imagen: carcassonne
   },
 
   {
@@ -23,7 +43,8 @@ export const PRODUCTOS: Producto[] = [
     categoria: 'Accesorios',
     nombre: 'Controlador Inalámbrico Xbox Series X',
     precio: 59990,
-    descripcion: 'Ofrece una experiencia de juego cómoda con botones mapeables y una respuesta táctil mejorada. Compatible con consolas Xbox y PC.'
+    descripcion: 'Ofrece una experiencia de juego cómoda con botones mapeables y una respuesta táctil mejorada. Compatible con consolas Xbox y PC.',
+    imagen: mandoXbox
   },
 
   {
@@ -31,7 +52,8 @@ export const PRODUCTOS: Producto[] = [
     categoria: 'Consolas',
     nombre: 'PlayStation 5',
     precio: 549990,
-    descripcion: 'La consola de última generación de Sony, que ofrece gráficos impresionantes y tiempos de carga ultrarrápidos para una experiencia de juego inmersiva.'
+    descripcion: 'La consola de última generación de Sony, que ofrece gráficos impresionantes y tiempos de carga ultrarrápidos para una experiencia de juego inmersiva.',
+    imagen: ps5
   },
 
   {
@@ -39,7 +61,8 @@ export const PRODUCTOS: Producto[] = [
     categoria: 'Computadores Gamers',
     nombre: 'PC Gamer ASUS ROG Strix',
     precio: 1299990,
-    descripcion: 'Un potente equipo diseñado para los gamers más exigentes, equipado con los últimos componentes para ofrecer un rendimiento excepcional en cualquier juego.'
+    descripcion: 'Un potente equipo diseñado para los gamers más exigentes, equipado con los últimos componentes para ofrecer un rendimiento excepcional en cualquier juego.',
+    imagen: pc
   },
 
   {
@@ -47,7 +70,8 @@ export const PRODUCTOS: Producto[] = [
     categoria: 'Sillas Gamers',
     nombre: 'Silla Gamer Secretlab Titan',
     precio: 349990,
-    descripcion: 'Diseñada para el máximo confort, esta silla ofrece un soporte ergonómico y personalización ajustable para sesiones de juego prolongadas.'
+    descripcion: 'Diseñada para el máximo confort, esta silla ofrece un soporte ergonómico y personalización ajustable para sesiones de juego prolongadas.',
+    imagen: silla
   },
   
   {
@@ -55,6 +79,25 @@ export const PRODUCTOS: Producto[] = [
     categoria: 'Poleras Personalizadas',
     nombre: "Polera Gamer Personalizada 'Level-Up'",
     precio: 14990,
-    descripcion: 'Una camiseta cómoda y estilizada, con la posibilidad de personalizarla con tu gamer tag o diseño favorito.'
+    descripcion: 'Una camiseta cómoda y estilizada, con la posibilidad de personalizarla con tu gamer tag o diseño favorito.',
+    imagen: polera
+  },
+
+  {
+    codigo: 'MO001',
+    categoria: 'Mouse',
+    nombre: "Mouse Gamer Logitech G502 HERO",
+    precio: 49990,
+    descripcion: 'Con sensor de alta precisión y botones personalizables, este mouse es ideal para gamers que buscan un control preciso y personalización.',
+    imagen: mouse
+  },
+
+  {
+    codigo: 'MO002',
+    categoria: 'Mousepad',
+    nombre: "Mousepad Razer Goliathus Extended",
+    precio: 29990,
+    descripcion: 'Ofrece un área de juego amplia con iluminación RGB personalizable, asegurando una superficie suave y uniforme para el movimiento del mouse.',
+    imagen: mousepad
   }
 ];
